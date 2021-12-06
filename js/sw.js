6c95add6b790dfc1949349a6fc1ac864c62d855b
GovindRajeesh <69973781+GovindRajeesh@users.noreply.github.com>
2021-12-06 04:41:09 -0800

    sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,25 @@
-ss
+const CACHE_NAME = 'offline';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([
+      '/index.html','/script.js','/styles/components.css','/styles/index.css','/styles/animation.css','/icons/Computer.png','/icons/User.png']);
+  })());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }else if(event.request[0]=="/"){
+        return fetch("/index.html?url="+event.request)
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
